f5f3d2db5fde9680a3fadd43aab9e9a44f592854
MuthulakshmiNagarajan <36658873+MuthulakshmiNagarajan@users.noreply.github.com>
2018-02-20 15:04:55 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,10 @@
+a=int(raw_input())
+b=int(raw_input())
+c=int(raw_input())
+if(a>b and a>c):
+    print("the greatest number is "+str(a))
+elif(b>c):
+    print("the greatest number is "+str(b))
+else:
+    print("the greatest number is "+str(c))
+    
